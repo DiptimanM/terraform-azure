fdf480b40369bfc3a83957996f15de31d3222bee
Diptiman <diptiman1985@gmail.com>
2022-07-27 19:21:21 +0000

    Upload VNet, SNET - attempt 4

--- vnet-subnet.tf
+++ vnet-subnet.tf
@@ -11,6 +11,4 @@ resource "azurerm_subnet" "myterraformsubnet"{
     resource_group_name = azurerm_resource_group.myTerraformGroup.name
     virtual_network_name = azurerm_virtual_network.myterraformnetwork.name
     address_prefixes = ["10.0.0.0/24"]
-    tags = var.tags
-
 }
