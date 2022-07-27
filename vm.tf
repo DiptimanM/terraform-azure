1e88b56173ca10208cc3655eb68abe72049dff41
Diptiman <diptiman1985@gmail.com>
2022-07-27 20:42:43 +0000

    Upload VM

---
+++ vm.tf
@@ -0,0 +1,30 @@
+resource "azurerm_linux_virtual_machine" "myterraformvm" {
+  name                = var.vmname
+  resource_group_name = azurerm_resource_group.myTerraformGroup.name
+  location            = var.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.myterraformnic.id
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = tls_private_key.ssh-key.ssh-key.public_key_openssh
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+
+  computer_name = var.vmname
+  disable_password_authentication = true
+}
